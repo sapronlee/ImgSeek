d2d0ae394ad2db0879bdd50d55b7b828168f59a0
zh.yang <howiloveyou@126.com>
2012-12-22 15:32:37 +0800

    modified:   app/controllers/api/v2/pictures_controller.rb 	modified:   app/models/picture.rb 	modified:   app/models/scenic.rb 	modified:   config/initializers/default_settings.rb 	modified:   config/locales/admin.zh-CN.yml

--- app/controllers/api/v2/pictures_controller.rb
+++ app/controllers/api/v2/pictures_controller.rb
@@ -4,14 +4,8 @@ class Api::V2::PicturesController < Api::V2::ApplicationController
     param = { :scenic_id => params[:scenic], :image => params[:image] }
     @picture = Picture.new param
     if @picture.save
-      result = [] 
-      pics = @picture.find_by_feature
-=begin
-      p_arr = @picture.find_by_path
-      p_arr.each do |p|
-        result << Picture.find(p).place
-      end
-=end
+      result = []
+      pics = @picture.find_by_feature(param[:scenic_id])
       pics.each{|o| result << o.place }
       result.uniq!
     end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -10,73 +10,46 @@ class Picture < ActiveRecord::Base
   
   # filter
   before_save :update_image_attributes, :update_place_attributes
-  #after_save :add_image_to_imgseek
-  #after_destroy :remove_image_to_imgseek
   
   # Validates
   validates :image, :scenic_id, :presence => true
-  
-  # result is pictrue.id array
-   def find_by_path
-    result = []
-    server_result = Server.find_image(scenic_id, image.path)
-    server_result.each do |i|
-      result << i.first if i.last > Setting.similarity
-    end
-    result
-  end
-  
-   def find_by_feature()
-    results = []
-    rs = []
-    stored_pictures = Picture.where("place_id!=0")
-    stored_pictures.each do |i|
-      feature_cnt = PictureMatch.match_pic_feature(sig, siglen, i.sig, i.siglen)
-      result = {:fcount => feature_cnt, :pic => i}
-      results << result
-    end
-    results = results.sort_by{|e| e[:fcount]}.reverse
-    satisfied_cnt = results.find_all{|e| e[:fcount] > Setting.similarity}.length
-    satisfied_cnt.zero? ? rs << results.first : results.each{|x| rs << x[:pic]}
-    rs
+
+   def find_by_feature(scenic_id)
+     begin
+      results = []
+      rs = []
+      stored_pictures = Picture.where("place_id!=0 AND scenic_id=?", scenic_id)
+      stored_pictures.each do |i|
+        feature_cnt = PictureMatch.match_pic_feature(sig, siglen, i.sig, i.siglen)
+        result = {:fcount => feature_cnt, :pic => i}
+        results << result
+      end
+      results = results.sort_by{|e| e[:fcount]}.reverse
+      satisfied_cnt = results.find_all{|e| e[:fcount] > Setting.threshold}.length
+      satisfied_cnt.zero? ? results.each{|x| rs << x[:pic] } : rs << results.first[:pic]
+      rs.take(5)
+     rescue
+      raise "imgseeks.server.img_import_error"
+     end
   end
   
   private
   def update_image_attributes
-    if image.present? && image_changed?
-      self.image_type = MIME::Types.type_for(image.file.original_filename).first.to_s
-      self.image_size = image.file.size
-      self.title = image.file.basename.strip if title.blank?
-      r = PictureMatch.get_pic_feature(image.path)
-      self.sig = r[0]
-      self.siglen = r[1]
+    begin
+      if image.present? && image_changed?
+        self.image_type = MIME::Types.type_for(image.file.original_filename).first.to_s
+        self.image_size = image.file.size
+        self.title = image.file.basename.strip if title.blank?
+        r = PictureMatch.get_pic_feature(image.path)
+        self.sig = r[0]
+        self.siglen = r[1]
+      end
+    rescue
+      raise "imgseeks.server.img_import_error"
     end
   end
   
   def update_place_attributes
     self.place_id = 0 if self.place_id.blank?
   end
-=begin
-  def remove_image_to_imgseek
-    begin
-      if !place_id.zero?
-        Server.remove_image(scenic_id, id)
-        Server.save_db(scenic_id)
-      end
-    rescue
-      raise "imgseeks.server.error"
-    end
-  end
-  
-  def add_image_to_imgseek
-    begin
-      if !place_id.zero?
-        Server.add_image(scenic_id, id, image.path)
-        Server.save_db(scenic_id)
-      end
-    rescue
-      raise "imgseeks.server.error"
-    end
-  end
-=end
 end
